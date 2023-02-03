2157fbb7b2094950edd94cb0fb74bcbdb064b250
janis.zhang <janis.zhang@liferay.com>
2023-02-03 21:44:17 +0800

    Merge_Sorted_Array

--- src/main/java/com/notAC/array/Merge_Sorted_Array.java
+++ src/main/java/com/notAC/array/Merge_Sorted_Array.java
@@ -3,12 +3,18 @@ package com.notAC.array;
 /**
  * @author JiaDi Zhang
  * @LeetCode: 88. Merge Sorted Array
- * @Link: https://leetcode.com/problems/merge-sorted-array/
+ * https://leetcode.com/problems/merge-sorted-array/
+ * https://leetcode.com/problems/merge-sorted-array/solutions/2120234/visual-explanation-o-1-space-java/
  * @date 2023/2/3
  */
 public class Merge_Sorted_Array {
-
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-
+    public static void merge(int[] A, int m, int[] B, int n) {
+        for ( int i = m+n-1, a = m - 1, b = n - 1;b >= 0;i--) {
+            if ((a >= 0) && (A[a] > B[b])) {
+                A[i] = A[a--];
+                continue;
+            }
+            A[i] = B[b--];
+        }
     }
 }
