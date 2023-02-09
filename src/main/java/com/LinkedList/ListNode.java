da3732a61c7961319d1e95857d6917839498a1fa
janis.zhang <janis.zhang@liferay.com>
2023-02-09 17:04:01 +0800

    Merge Two Sorted Lists and refactor

--- src/main/java/com/LinkedList/ListNode.java
+++ src/main/java/com/LinkedList/ListNode.java
@@ -8,39 +8,9 @@ package com.LinkedList;
  * @date 2023/2/8
  */
 class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) {
-          val = x;
-         next = null;
-     }
-
-    /**
-     * fast-slow method
-     * @param head
-     */
-    public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast != null) {
-           slow = slow.next;
-           fast = fast.next.next;
-           if (slow == fast) {
-               return true;
-           }
-        }
-        return false;
-    }
-
-    public boolean hasCycle1(ListNode head) {
-          while (head.next != null) {
-              if (head.next.val ==  100000) {
-                  return true;
-              }
-              head.next.val = 100000;
-              head = head.next;
-          }
-          return false;
-    }
-
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
