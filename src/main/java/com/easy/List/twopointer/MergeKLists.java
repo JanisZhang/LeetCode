7ec65bf6c79fdadaebb5e98dac03d43a201c8b41
JiadiZhang <janiszhang.se@gmail.com>
2024-07-30 15:42:05 +0800

    //To-do MergeKLists

---
+++ src/main/java/com/easy/List/twopointer/MergeKLists.java
@@ -0,0 +1,30 @@
+package com.easy.List.twopointer;
+
+/**
+ * @link: https://leetcode.com/problems/merge-k-sorted-lists/
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ *     Output: [1,1,2,3,4,4,5,6]
+ *     Explanation: The linked-lists are:
+ *             [
+ *             1->4->5,
+ *             1->3->4,
+ *             2->6
+ *             ]
+ *     merging them into one sorted list:
+ *             1->1->2->3->4->4->5->6
+ */
+public class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        return null;
+    }
+
+    static class ListNode {
+        int val;
+        MergeTwoSortedLists.ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, MergeTwoSortedLists.ListNode next) { this.val = val; this.next = next; }
+    }
+}
