70300c7aa56ef49e3422e26862d43d44e2ab6bd1
JiadiZhang <janiszhang.se@gmail.com>
2024-07-31 13:39:33 +0800

    141. Linked List Cycle

---
+++ src/main/java/com/easy/List/twopointer/HasCycle.java
@@ -0,0 +1,22 @@
+package com.easy.List.twopointer;
+
+/**
+ * @link; https://leetcode.com/problems/linked-list-cycle/
+ */
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode p1 = head;
+        if(p1 == null||p1.next == null){
+            return false;
+        }
+        ListNode p2 = head;
+        while(p2.next!=null&&p2.next.next!=null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if(p1 == p2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
