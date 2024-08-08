344ebc1e45c0a99284bd72ccee61109632e4f1cd
JiadiZhang <janiszhang.se@gmail.com>
2024-08-08 10:15:39 +0800

    refactor

--- src/main/java/com/easy/List/ReverseLinkedList.java
+++ src/main/java/com/easy/List/ReverseLinkedList.java
@@ -1,6 +1,8 @@
 package com.easy.List;
 
 
+import com.easy.List.twopointer.PartitionList;
+
 /**
  * @author JiaDi Zhang
  * @LeetCode: 206. Reverse Linked List
@@ -14,15 +16,39 @@ public class ReverseLinkedList {
      * @param head
      * @return
      */
-    public static ListNode reverseList(ListNode head) {
-        ListNode currentNode = head;
-        ListNode previousHead = null;
-        while (currentNode != null ){
-            ListNode next = currentNode.next;
-            currentNode.next = previousHead;
-            previousHead = currentNode;
-            currentNode = next;
+//    public static ListNode reverseList(ListNode head) {
+//        ListNode currentNode = head;
+//        ListNode previousHead = null;
+//        while (currentNode != null ){
+//            ListNode next = currentNode.next;
+//            currentNode.next = previousHead;
+//            previousHead = currentNode;
+//            currentNode = next;
+//        }
+//        return previousHead;
+//    }
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
-        return previousHead;
+        ListNode last = reverse(head.next);
+
+        head.next.next = head;
+
+        head.next = null;
+
+        return last;
     }
+
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node = new ListNode(1, node2);
+
+        reverse(node);
+    }
+    
+    
 }
