a6574114b1051357adb23a0aeaf5694a07b9ddcf
JiadiZhang <janiszhang.se@gmail.com>
2024-08-09 14:23:53 +0800

    567. Permutation in String

---
+++ src/main/java/com/easy/string/slidingwindow/PermutationInString.java
@@ -0,0 +1,55 @@
+package com.easy.string.slidingwindow;
+
+import java.util.HashMap;
+
+/**
+ * @link: https://leetcode.com/problems/permutation-in-string/description/
+ */
+public class PermutationInString {
+    public static boolean checkInclusion(String s1, String s2) {
+        HashMap<Character,Integer> windows = new HashMap<>();
+        HashMap<Character,Integer> need = new HashMap<>();
+
+        for (char cur:s1.toCharArray()) {
+            need.put(cur,need.getOrDefault(cur,0)+1);
+        }
+
+        int left = 0, right =0;
+        int valid = 0;
+
+        while (right < s2.length()) {
+            char cur = s2.charAt(right);
+            right++;
+
+            if (need.containsKey(cur)) {
+                windows.put(cur, windows.getOrDefault(cur,0)+1);
+                if(windows.get(cur).equals(need.get(cur))){
+                    valid++;
+                }
+            }
+
+            while (valid == need.size()) {
+                char del = s2.charAt(left);
+                left++;
+
+                if(need.containsKey(del)){
+                    if(right - left == s1.length()-1) {
+                        return true;
+                    }
+
+                    if(windows.get(del).equals(need.get(del))){
+                        valid--;
+                    }
+                    windows.put(del, windows.get(del)-1);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "adc", s2 = "dcda";
+        System.out.println(checkInclusion(s1,s2));
+    }
+}
