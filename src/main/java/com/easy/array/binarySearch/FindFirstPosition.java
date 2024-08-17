f9a221ee15ac500b419ac4f518e227fb4706636b
JiadiZhang <janiszhang.se@gmail.com>
2024-08-17 08:49:50 +0800

    二分搜索左侧边界（左闭右闭）

--- src/main/java/com/easy/array/binarySearch/FindFirstPosition.java
+++ src/main/java/com/easy/array/binarySearch/FindFirstPosition.java
@@ -2,14 +2,14 @@ package com.easy.array.binarySearch;
 
 public class FindFirstPosition {
     private static int search(int[] nums, int target) {
-        int left = 0, right = nums.length;
+        int left = 0, right = nums.length - 1;
         while (left < right) {
             int mid = left + (right - left)/2;
 
             if (target == nums[mid]) {
-                right = mid;
+                right = mid - 1;
             } else if (target < nums[mid]) {
-                right = mid;
+                right = mid - 1;
             } else if (target > nums[mid]) {
                 left = mid + 1;
             }
